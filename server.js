1654ea57e89ffc8c10a2bbde6cc643012d469970
Miles Florence <milesflo15@gmail.com>
2015-12-22 15:26:21 -0800

    Initial Commit

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 // Your Server Code Here
+
+app.get('/search/:id', function(req,res) {
+	res.render("search.ejs");
+});
+
+app.get('/search/', function(req,res) {
+	res.render("search.ejs");
+});
+
+app.get("/", function(req,res) {
+	res.redirect("/search");
+})
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
